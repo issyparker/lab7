9d5d31567ce9021318e4b7f88d30f67666cc5e34
Isabelle Parker <iparker@ucsd.edu>
2021-02-18 13:13:56 +1000

    changed

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeCtr').click(recordClick);
+	$('.likeBtn').click(recordClick);
+}
+
+function recordClick(e) {
+	e.preventDefault();
+	ga("send", "event", 'like', 'click');
 }
